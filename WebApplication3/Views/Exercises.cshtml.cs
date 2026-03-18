55200128da84edd5b228a511cce4a7397158b137
Kylewercoir <Kylewercoir@gmail.com>
2026-03-18 18:01:07 -0400

    fixed problems

---
+++ WebApplication3/Views/Exercises.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3.Views
+{
+    public class ExercisesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
